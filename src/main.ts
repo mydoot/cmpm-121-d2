ee47c232971ebefd150fdf556a4b0bba42179922
mydoot <buaqur@gmail.com>
2025-10-17 02:22:47 +0000

    Added drawing capabilities (Completed Step 2)
    Co-authored-by: BeReyes1 <BeReyes1@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,35 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.className = "canvas";
 document.body.appendChild(canvas);
+
+const cursor = { active: false, x: 0, y: 0 };
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+canvas.addEventListener("mousedown", (event) => {
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (cursor.active && ctx != null) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+  }
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
